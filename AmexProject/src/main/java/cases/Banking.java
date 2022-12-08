3bd0b522e4e75f318adfec4c21b5893714055784
Valmiki Vishwakarma <you@example.com>
2022-12-08 22:20:58 +0530

    Code commit.

---
+++ AmexProject/src/main/java/cases/Banking.java
@@ -0,0 +1,14 @@
+package cases;
+
+import Basics.Calculator;
+
+public class Banking {
+	
+	public static void main(String[] args) {
+		Calculator ca = new Calculator(1);
+		ca.add();
+		ca.mult();
+
+	}
+
+}
